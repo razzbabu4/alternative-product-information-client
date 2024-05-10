311cf7e0077d04ac05a4cb0807b1cf255c336a27
Tareq <razzbabut@gmail.com>
2024-05-11 00:32:11 +0600

    complete email-password & google login successfully

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,50 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import login from '../../public/login.svg';
-// import useAuth from '../hooks/useAuth';
-// import {toast} from "react-toastify"
+import useAuth from '../hooks/useAuth';
+import { toast } from "react-toastify";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
-   
+    const { signIn, user, googleLogin } = useAuth();
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleLogin = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password)
+        if (user) {
+            toast.warning('Already an user exist')
+            return;
+        }
+        signIn(email, password)
+            .then((result) => {
+                console.log(result.user)
+                toast.success('Successfully login')
+                navigate(location?.state ? location.state : '/')
+            })
+            .catch(() => {
+                toast.warning('Invalid email or password')
+            });
+
+        form.email.value = "";
+        form.password.value = "";
+    }
+
+    const handleGoogleLogin = () => {
+        googleLogin()
+            .then(result => {
+                if (result.user) {
+                    navigate(location?.state || '/')
+                }
+            })
+            .catch(error => {
+                console.error(error.message)
+            })
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
@@ -17,13 +57,13 @@ const Login = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
@@ -38,9 +78,9 @@ const Login = () => {
                         <div className="flex-grow h-px  dark:bg-gray-300"></div>
                     </div>
                     <div className="mt-6">
-                            <button className="btn btn-block">Google</button>
-                        </div>
-                    <p className="text-xs text-center sm:px-6 dark:text-gray-600 ">Do not have an account? 
+                        <button onClick={handleGoogleLogin} className="btn btn-block bg-gray-200"><FcGoogle className="text-2xl"></FcGoogle> Google</button>
+                    </div>
+                    <p className="text-xs text-center sm:px-6 dark:text-gray-600 ">Do not have an account?
                         <Link to='/register' className="text-base text-violet-700"> Register</Link>
                     </p>
                 </div>
